9fd3fcb57517d25c9cfb9d4a51a626a1bf45190f
Sagar <sagarchandgadkar74@gmail.com>
2021-02-15 14:47:14 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,41 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+import Heading from "./Heading";
+import InputQuery from "./InputQuery"
+import SubHeading from "./SubHeading"
+import SubmitButton from "./SubmitButton"
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <div id="main">
+//       <Heading />
+//       <SubHeading />
+//       <InputQuery />
+//       <SubmitButton />
+//     </div>
+//   )
+// }
+// export default App;
+
+
+
+
+function App() {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <h1>App</h1>
+      <Heading />
+      <SubHeading />
+      <InputQuery />
+      <SubmitButton />
     </div>
   )
 }
+
+export default App
+
+
+
+
 
 
